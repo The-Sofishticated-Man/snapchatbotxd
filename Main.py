48654a5ace74080f523551b97406a3da5519b695
islam <lameche1islam@gmail.com>
2024-08-24 16:12:27 +0100

    added automatic login functionality

---
+++ Main.py
@@ -0,0 +1,30 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+USERNMAME = "hmaloney2024"
+PASSWORD = "mal51159"
+
+driver.get("https://snapchat.com/")
+username = driver.find_element(By.ID, "ai_input")
+username.send_keys(USERNMAME + Keys.ENTER)
+time.sleep(5)
+password = driver.find_element(By.NAME, "password")
+password.send_keys(PASSWORD + Keys.ENTER)
+time.sleep(20)
+
+try:
+    notificationPopUp = driver.find_element(By.CLASS_NAME, "gIloE")
+    closeButton = driver.find_element(By.CLASS_NAME, "cV8g1")
+    closeButton.click()
+except NoSuchElementException:
+    pass
+
