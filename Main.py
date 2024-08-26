34c41cc94e061af51ccc8d663ff03351220945ed
islam <lameche1islam@gmail.com>
2024-08-27 00:17:46 +0100

    added selection state for the victim class

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
+from Victim import Victim
+
 import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -19,13 +22,26 @@ username.send_keys(USERNMAME + Keys.ENTER)
 time.sleep(5)
 password = driver.find_element(By.NAME, "password")
 password.send_keys(PASSWORD + Keys.ENTER)
+try:
+    errorPopUp = driver.find_element(By.CLASS_NAME, "O1Map")
+    driver.refresh()
+except NoSuchElementException:
+    pass
 time.sleep(10)
+
 try:
     notificationPopUp = driver.find_element(By.CLASS_NAME, "gIloE")
     closeButton = driver.find_element(By.CLASS_NAME, "cV8g1")
     closeButton.click()
 except NoSuchElementException:
     pass
-victimElements = driver.find_elements(By.CLASS_NAME, "O4POs")
+time.sleep(4)
 
-    
+
+time.sleep(4)
+
+victimElements = driver.find_elements(By.CLASS_NAME, "O4POs")
+victims = [Victim(victimElement, driver) for victimElement in victimElements]
+
+victims[0].sendMessage("hi there my name is hmaloney2024")
+victims[1].sendMessage("hi there my name is john cena ")
--- Victim.py
+++ Victim.py
@@ -1,42 +1,39 @@
+import time
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
 class Victim:
+    selectedInstance = None
     def __init__(self, victimElement: WebElement, driver: WebDriver) -> None:
         self.element = victimElement
         self.context = driver
-        self._name = self.element.find_element(By.CSS_SELECTOR, "span .nonIntl").text
-        self._isSelected = False
-
+    
     @property
     def name(self):
-        self._name = self.element.find_element(By.CSS_SELECTOR, "span .nonIntl").text
-        return self._name
+        return self.element.find_element(By.CSS_SELECTOR, "span .nonIntl").text
 
     @property
     def isSelected(self):
-        return self._isSelected
+        return self == Victim.selectedInstance 
 
     def select(self):
-        if not self._isSelected:
+        if not self.isSelected:
             self.element.click()
-            self._isSelected = True
+            Victim.selectedInstance = self
 
     def unselect(self):
-        if self._isSelected:
-            try:
-                victimBar = self.context.find_element(By.CLASS_NAME, "k1IaM")
-                closeBtn = victimBar.find_element(By.CLASS_NAME, "kY4Ky BWtqT")
-            except NoSuchElementException:
-                self._isSelected = False
-            else:
-                closeBtn.click()
-                self._isSelected = False
+        if self.isSelected:
+            victimBar = self.context.find_element(By.CLASS_NAME, "k1IaM")
+            closeBtn = victimBar.find_element(By.CLASS_NAME, "kY4Ky BWtqT")
+            closeBtn.click()
+            Victim.selectedInstance = None
+
     def sendMessage(self, message: str):
+        time.sleep(0.5)
         if not self.isSelected:
             self.select()
         messageBar = self.context.find_element(By.CSS_SELECTOR, "div[role='textbox']")
