ad7b853c2531abeda55349f85ad718200f3b099a
islam <lameche1islam@gmail.com>
2024-08-27 14:29:42 +0100

    added explicit waits to avoid race conditions

--- Client.py
+++ Client.py
@@ -6,7 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 
@@ -15,18 +16,21 @@ class Client:
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=chrome_options)
+        self.wait = WebDriverWait(self.driver, 15)
 
     def login(self, username: str, password: str):
         self.driver.get("https://snapchat.com/")
 
-        # find username field and enter username
-        usernameInput = self.driver.find_element(By.ID, "ai_input")
-        usernameInput.send_keys(username + Keys.ENTER)
-        time.sleep(5)
+        # wait for username field to appear, find username field and enter username
+        self.wait.until(EC.element_to_be_clickable((By.ID, "ai_input"))).send_keys(
+            username + Keys.ENTER
+        )
 
-        # find password field and enter password
-        passwordInput = self.driver.find_element(By.NAME, "password")
-        passwordInput.send_keys(password + Keys.ENTER)
+        # wait until password field appears, find password field and enter password
+        self.wait.until(
+            EC.presence_of_element_located((By.NAME, "password"))
+        ).send_keys(password + Keys.ENTER)
+
         while True:
             # check if there is an error pop up and refresh if there is
             try:
@@ -36,7 +40,7 @@ class Client:
             else:
                 self.driver.refresh()
 
-            # check if there is a notification pop up signifying successful login and close it if there is
+            # check if there is a notification pop up signifying successful login and close it and exit the loop
             try:
                 closeButton = self.driver.find_element(By.CLASS_NAME, "cV8g1")
             except NoSuchElementException:
@@ -44,17 +48,17 @@ class Client:
             else:
                 closeButton.click()
                 break
+        print("successfully logged in")
 
     def loadVictims(self):
-        time.sleep(3)
         while True:
             try:
-                victimElements = self.driver.find_elements(By.CLASS_NAME, "O4POs")
-            except NoSuchElementException:
-                time.sleep(0.5)
-                print("couldn't find victims")
+                victimElements = self.wait.until(
+                    EC.presence_of_all_elements_located((By.CLASS_NAME, "O4POs"))
+                )
+            except TimeoutError:
+                self.driver.refresh()
             else:
-                print(f"found {len(victimElements)} victims")
                 self.victims = [
                     Victim(victimElement, self.driver)
                     for victimElement in victimElements
--- Victim.py
+++ Victim.py
@@ -3,22 +3,30 @@ import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
 class Victim:
     selectedInstance = None
+
     def __init__(self, victimElement: WebElement, driver: WebDriver) -> None:
         self.element = victimElement
         self.context = driver
-    
+        self.wait = WebDriverWait(driver, 5)
+
     @property
     def name(self):
-        return self.element.find_element(By.CSS_SELECTOR, "span .nonIntl").text
+        name = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "span .nonIntl"))
+        ).text
+        return name
 
     @property
     def isSelected(self):
-        return self == Victim.selectedInstance 
+        return self == Victim.selectedInstance
 
     def select(self):
         if not self.isSelected:
@@ -27,15 +35,18 @@ class Victim:
 
     def unselect(self):
         if self.isSelected:
-            victimBar = self.context.find_element(By.CLASS_NAME, "k1IaM")
+            victimBar = self.wait.until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "k1IaM"))
+            )
             closeBtn = victimBar.find_element(By.CLASS_NAME, "kY4Ky BWtqT")
             closeBtn.click()
             Victim.selectedInstance = None
 
     def sendMessage(self, message: str):
-        time.sleep(0.5)
         if not self.isSelected:
             self.select()
-        messageBar = self.context.find_element(By.CSS_SELECTOR, "div[role='textbox']")
+        messageBar = self.wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "div[role='textbox']"))
+        )
         messageBar.click()
         messageBar.send_keys(message + Keys.ENTER)
